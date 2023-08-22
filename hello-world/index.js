8019225f326086712677e792c69974d2c590349b
Bikumalla Sreeja <sreejabikumalla04@gmail.com>
2023-08-22 21:24:23 +0530

    Exploring fs module

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,10 @@
-const hello = () => {
-    console.log("Hello GitHub!");
-  };
-  
-  hello()
+const { Console } = require("console");
+const fs = require("fs");
+fs.writeFile(
+  "sample.txt",
+  "Hello World",
+  (err)=>{
+    if(err) throw err;
+    console.log("File created!");
+  }
+);
