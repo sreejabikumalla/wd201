0e4900e433ca48f0730fd51fb4e3d10ad30d5571
Bikumalla Sreeja <sreejabikumalla04@gmail.com>
2023-08-23 09:45:34 +0530

    Exploring streams

--- http-server/index.js
+++ http-server/index.js
@@ -1,10 +1,22 @@
-const { Console } = require("console");
+// const { Console } = require("console");
+// const fs = require("fs");
+// fs.writeFile(
+//   "sample.txt",
+//   "Hello World",
+//   (err)=>{
+//     if(err) throw err;
+//     console.log("File created!");
+//   }
+// );
+
+const http= require("http");
 const fs = require("fs");
-fs.writeFile(
-  "sample.txt",
-  "Hello World",
-  (err)=>{
-    if(err) throw err;
-    console.log("File created!");
-  }
-);
+
+const server = http.createServer((req,res) => {
+  const stream = fs.createReadStream("sample.txt");
+  stream.pipe(res);
+  // fs.readFile("sample.txt",(err,data)=>{
+  //   res.end(data);
+  // })
+});
+server.listen(3000);
